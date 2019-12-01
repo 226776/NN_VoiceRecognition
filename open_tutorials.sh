8a36a46ab2aa1ceb8928e3164d3a7c38a9867aea
Diith <48160583+226776@users.noreply.github.com>
2019-12-01 12:39:56 +0100

    Add files via upload

---
+++ open_tutorials.sh
@@ -0,0 +1,12 @@
+firefox https://course.fast.ai/part2 &
+sleep 2
+
+firefox -new-tab https://pytorch.org/tutorials/beginner/nn_tutorial.html
+
+firefox -new-tab https://pytorch.org/tutorials/beginner/blitz/neural_networks_tutorial.html#sphx-glr-beginner-blitz-neural-networks-tutorial-py
+
+firefox -new-tab https://pytorch.org/tutorials/beginner/pytorch_with_examples.html
+  
+firefox -new-tab https://software.intel.com/en-us/ai/courses
+
+jupyter notebook
